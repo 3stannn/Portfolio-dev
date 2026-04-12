632ec5cbc04e8a43d7ff70e313f083a2704049d3
Thristan <thristandave12@gmail.com>
2026-04-12 14:41:45 +0800

    improved theme button

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function themeFunction() {
-   var element = document.body;
-   element.classList.toggle("light-mode");
+   var body = document.body;
+   body.classList.toggle("light-mode");
+   const icon = document.getElementById("theme-icon");
+   if (!icon) {
+      return;
+   }
+   if (body.classList.contains("light-mode")) {
+      icon.src = "img/moon.png";
+      icon.alt = "Moon icon";
+   } else {
+      icon.src = "img/sun.png";
+      icon.alt = "Sun icon";
+   }
 }
